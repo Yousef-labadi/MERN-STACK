f5b3b37987a605d627ff68aafaeb447673ba9cc4
Yousef-Labadi <jozeflabadi@gmail.com>
2023-06-08 15:41:21 +0300

    Update user form add some validation

--- Hook_Form/app1/src/components/UserForm.jsx
+++ Hook_Form/app1/src/components/UserForm.jsx
@@ -6,12 +6,70 @@ const UserForm = (props) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [cpassword, setCpassword] = useState('');
+  const [fnameError, setFnameError] = useState("");
+  const [lnameError, setlnameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [cpasswordError, setCPasswordError] = useState("");
+  
 
   const createUser = (e) => {
     e.preventDefault();
-    const newUser = { fname, lname, email, password, cpassword };
-    console.log('Welcome', newUser);
+    const newUser = {  lname, email, password, cpassword };
+    
   };
+  const handleFname = (e) => {
+    setfname(e.target.value);
+    if(e.target.value.length < 1) {
+        setFnameError("First Name is required!");
+    } else if(e.target.value.length < 2) {
+        setFnameError("First Name must be 2 characters or longer!");
+    } else {
+        setFnameError("");
+    }
+}
+const handlelname = (e) => {
+    setlname(e.target.value);
+    if(e.target.value.length < 1) {
+        setlnameError("Last Name is required!");
+    } else if(e.target.value.length < 2) {
+        setlnameError("Last Name must be 2 characters or longer!");
+    } else {
+        setlnameError("");
+    }
+}
+const handleEmail = (e) => {
+    setEmail(e.target.value);
+    if(e.target.value.length < 1) {
+        setEmailError("Email is required!");
+    } else if(e.target.value.length < 2) {
+        setEmailError("Email must be 2 characters or longer!");
+    } else {
+        setEmailError("");
+    }
+}
+const handlePassword = (e) => {
+    setPassword(e.target.value);
+    if(e.target.value.length < 1) {
+        setPasswordError("Password is required!");
+    } else if(e.target.value.length < 8) {
+        setPasswordError("Password must be 8 characters or longer!");
+    } else {
+        setPasswordError("");
+    }
+}
+const handleCPassword = (e) => {
+    setCpassword(e.target.value);
+    if (e.target.value.length < 1) {
+        setCPasswordError("Confirm Password is required!");
+    } else if (e.target.value.length < 8) {
+        setCPasswordError("Confirm Password must be 8 characters or longer!");
+    } else if (e.target.value !== password) {
+        setCPasswordError("Passwords must match");
+    } else {
+        setCPasswordError("");
+    }
+}
 
   return (
     <div className="form-container">
@@ -22,44 +80,63 @@ const UserForm = (props) => {
             type="text"
             className="form-input"
             value={fname}
-            onChange={(e) => setfname(e.target.value)}
+            onChange={handleFname}
           />
+          {
+            
+            <p style={{color:'red'}}>{ fnameError }</p>
+          }
         </div>
         <div className="form-group">
           <label className="form-label">Last Name:</label>
           <input
             type="text"
             className="form-input"
-            value={lname}
-            onChange={(e) => setlname(e.target.value)}
+            
+            onChange={handlelname}
           />
+          {
+            
+            <p style={{color:'red'}}>{ lnameError }</p>
+          }
         </div>
         <div className="form-group">
           <label className="form-label">Email Address:</label>
           <input
             type="email"
             className="form-input"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            
+            onChange={handleEmail}
           />
+          {
+            <p style={{color:'red'}}>{ emailError }</p>
+            
+          }
         </div>
         <div className="form-group">
           <label className="form-label">Password:</label>
           <input
             type="password"
             className="form-input"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            
+            onChange={handlePassword}
           />
+          {
+            <p style={{color:'red'}}>{ passwordError }</p>
+            
+          }
         </div>
         <div className="form-group">
           <label className="form-label">Confirm Password:</label>
           <input
             type="password"
             className="form-input"
-            value={cpassword}
-            onChange={(e) => setCpassword(e.target.value)}
+            
+            onChange={handleCPassword}
           />
+          {
+            <p style={{color:'red'}}>{ cpasswordError }</p>
+          }
         </div>
        
       </form>
