ebf47640dc4207a47578874357841ea5c0a7dbe9
Yousef-Labadi <jozeflabadi@gmail.com>
2023-06-08 13:50:59 +0300

    Big_Inversion

---
+++ Big_Inversion/app1/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+    <PersonCard firstName="Yousef"lastName="labadi" age={29}hairColor="black"  />
+    <PersonCard firstName="Hamzeh"lastName="Farsakh" age={27}hairColor="black"  />
+    <PersonCard firstName="Khalid"lastName="Hasouneh" age={28}hairColor="black"  />
+    <PersonCard firstName="Husam"lastName="Habayeb" age={28}hairColor="Yellow"  />
+    <PersonCard firstName="Mohammad"lastName="Shadid" age={26}hairColor="brown"  />
+    
+    </div>
+  );
+}
+
+export default App;
--- Prop It Up/app1/src/components/Mycom.jsx
+++ Prop It Up/app1/src/components/Mycom.jsx
@@ -4,29 +4,29 @@ import React, { Component } from 'react';
 
 class Mycom extends Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        age: this.props.age
-      };
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
     }
-  
+    
     IncrementAge = () => {
-      this.setState({age: this.state.age +1});
+        this.setState({ age: this.state.age + 1 });
     }
     DicrementAge = () => {
-        this.setState({age: this.state.age -1});
-      }
-  
-    render() {
-      return (
-        <div>
-          <h1>{this.props.first_name}, {this.props.last_name}</h1>
-          <p>Age: {this.state.age}</p>
-          <p>Hair Color: {this.props.color}</p>
-          <button onClick={this.IncrementAge} >Birthday Button for {this.props.first_name} {this.props.last_name} </button>
-          <button onClick={this.DicrementAge} >Birthday Button for {this.props.first_name} {this.props.last_name} </button>
-        </div>
-      );
+        this.setState({ age: this.state.age - 1 });
     }
-  }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.props.first_name}, {this.props.last_name}</h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color : {this.props.color}</p>
+                <button onClick={this.IncrementAge} >Birthday Button for {this.props.first_name} {this.props.last_name} </button>
+                <button onClick={this.DicrementAge} >Birthday Button for {this.props.first_name} {this.props.last_name} </button>
+            </div>
+        );
+    }
+}
 export default Mycom;
